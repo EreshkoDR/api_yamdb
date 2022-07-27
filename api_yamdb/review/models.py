2453c467f7b6dcf5f79247878b8f70cdf980a569
Екатерина Садыкова <tea-cea@yandex.ru>
2022-07-28 01:17:19 +0500

    start2

--- api_yamdb/review/models.py
+++ api_yamdb/review/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+# from django.db.models import UniqueConstraint
+from django.contrib.auth 
 
-# Create your models here.
+
+class Review(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    image = models.ImageField(
+        'Картинка',
+        upload_to='review/',
+        blank=True,
+        null=True,
+        help_text='Загрузите картинку'
+    )
+    text = models.TextField(
+        'Текст отзыва: ',
+        help_text='Напишите что-нибудь сюда...')
+    pub_date = models.DateTimeField('Дата публикации: ', auto_now_add=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='titles',
+    )
+    titles = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='titles',
+    )
+
+class Comment(CreatedModel):
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comments'
+        )
+    # author = models.ForeignKey(
+    #     User,
+    #     on_delete=models.CASCADE,
+    #     related_name='comments',
+    #     verbose_name='Автор комментария')
+    text = models.TextField(
+        'Текст коментария:',
+        help_text='Оставьте коментарий в поле')
+    created = models.DateTimeField('Дата коментария', auto_now_add=True)
+
+    def __str__(self):
+        return self.text[:15]
