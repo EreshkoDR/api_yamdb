9bd1098099a0c477ab02bdd49b852adb6ba58e0b
Екатерина Садыкова <tea-cea@yandex.ru>
2022-07-28 15:45:36 +0500

    start4

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -16,10 +16,18 @@ class ReviewSerializer(serializers.ModelSerializer):
             fields=['title', 'author']
         )
         ]
-        # тут проверка,что пользователь не может оставлять повторно ревью
-        def validate(self,value):
-            pass
 
+    def validate(self,value):
+        title_id = self.context['view'].kwargs.get('title_id')
+        author = self.context['request'].user
+        if Review.objects.filter(
+            author=author,title=title_id).exists():
+            raise serializers.ValidationError('У вас уже есть отзыв на это.')
+        return value
+
+    def validate_score(self, value):
+        if not 1 <= value <= 10:
+            raise serializers.ValidationError('Неверная оценка')
 class Comment(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(read_only=True,
                                           slug_field='username')
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -9,20 +9,20 @@ class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
 
     def perform_create(self, serializer):
-        comment = get_object_or_404(Comment, pk=self.kwargs.get('id'))
+        comment = get_object_or_404(Comment, pk=self.kwargs.get('review_id'))
         serializer.save(author=self.request.user, comment=comment)
 
     def get_queryset(self):
-        review = get_object_or_404(Review, pk=self.kwargs.get('id'))
+        review = get_object_or_404(Review, id=self.kwargs.get('review_id'))
         return review.comments
 
 class ReviewViewSet(viewsets.ModelViewSet):
     serializer_class = ReviewSerializer
 
     def perform_create(self, serializer):
-        review = get_object_or_404(Review, pk=self.kwargs.get('id'))
+        review = get_object_or_404(Review, pk=self.kwargs.get('title_id'))
         serializer.save(author=self.request.user, review=review)
 
     def get_queryset(self):
-        title = get_object_or_404(Title, pk=self.kwargs.get('id'))
+        title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
         return title.review
