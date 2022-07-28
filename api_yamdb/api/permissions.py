a427f793d39f7a83d4466f189332b4e12cb72421
Ereshko Danil <ereshko.d.r@gmail.com>
2022-07-28 19:54:42 +0500

    refactoring permissions

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -2,6 +2,7 @@ from rest_framework import permissions
 
 
 class IsAdminPermission(permissions.BasePermission):
+    """Разрешения уровня 'администратор'."""
 
     def has_permission(self, request, view):
         if request.user.is_authenticated:
@@ -21,16 +22,26 @@ class IsAdminPermission(permissions.BasePermission):
 
 
 class IsModeratorPermission(permissions.BasePermission):
-
+    """Разрешения уровня 'модератор'."""
     def has_permission(self, request, view):
-        return request.user.role == 'moderator'
+        if request.user.is_authenticated:
+            return (
+                request.user.role == 'moderator'
+                or request.user.is_superuser
+            )
+        return False
 
     def has_object_permission(self, request, view, obj):
-        return request.user.role == 'moderator'
+        if request.user.is_authenticated:
+            return (
+                request.user.role == 'moderator'
+                or request.user.is_superuser
+            )
+        return False
 
 
 class IsUserPermission(permissions.BasePermission):
-
+    """Разрешения уровня 'авторизированный пользователь'."""
     def has_permission(self, request, view):
         return request.user.is_authenticated
 
